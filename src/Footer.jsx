1f0a2a1abf3294f68c931ba5d92ac78f05ef5056
samrat-rock <samratpandey715@gmail.com>
2024-11-18 13:17:53 +0545

    vite lastest

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer className='bg-dark text-white p-4 text-center'>
+        footer
+      </footer>
+    </>
+  )
+}
+
+export default Footer
